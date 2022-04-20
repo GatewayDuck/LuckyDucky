5a6289d577b9c5a6182fa84834f1989a74f9f936
DuckInc <88541634+GatewayDuck@users.noreply.github.com>
2022-04-20 14:30:34 +0000

    Added init file to index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+require("dotenv").config()
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env['TOKEN'])
